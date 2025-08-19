c6c53a48743bc8320a7b83d16a3a7dc7ccad55f9
thebetterdaysforus-maker <thebetterdaysforus@gmail.com>
2025-08-19 14:40:09 +0900

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -39,7 +39,10 @@ export default ({ config }) => ({
   },
   extra: {
     EXPO_PUBLIC_SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL,
-    EXPO_PUBLIC_SUPABASE_ANON_KEY: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
+    EXPO_PUBLIC_SUPABASE_ANON_KEY: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+    eas: {
+      projectId: "thebetterday-mobile-app"
+    }
   },
   plugins: [
     [
