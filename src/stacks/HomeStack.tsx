826940538755b7497ac841720b57ab38c515cfa7
thebetterdaysforus-maker <thebetterdaysforus@gmail.com>
2025-08-20 14:40:10 +0900

    Update HomeStack.tsx

--- src/stacks/HomeStack.tsx
+++ src/stacks/HomeStack.tsx
@@ -1,46 +1,46 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
-import GoalBatchScreen from '../screens/GoalBatchScreen';
-import GoalDetailScreen from '../screens/GoalDetailScreen';
-import GoalListScreen from '../screens/GoalListScreen';
-import TimeSelectScreen from '../screens/TimeSelectScreen';
-import FlexibleGoalScreen from '../screens/FlexibleGoalScreen';
-import RetrospectScreen from '../screens/RetrospectScreen';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import GoalBatchScreen from "../screens/GoalBatchScreen";
+import GoalDetailScreen from "../screens/GoalDetailScreen";
+import GoalListScreen from "../screens/GoalListScreen";
+import TimeSelectScreen from "../screens/TimeSelectScreen";
+import FlexibleGoalScreen from "../screens/FlexibleGoalScreen";
+import RetrospectScreen from "../screens/RetrospectScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function HomeStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen 
-        name="GoalList" 
+      <Stack.Screen
+        name="GoalList"
         component={GoalListScreen}
         options={{ headerShown: false }}
       />
-      <Stack.Screen 
-        name="GoalDetail" 
+      <Stack.Screen
+        name="GoalDetail"
         component={GoalDetailScreen}
-        options={{ title: '목표 상세' }}
+        options={{ title: "수행 목록 상세" }}
       />
-      <Stack.Screen 
-        name="GoalBatch" 
+      <Stack.Screen
+        name="GoalBatch"
         component={GoalBatchScreen}
-        options={{ title: '수행 목록 설정' }}
+        options={{ title: "수행 목록 설정" }}
       />
-      <Stack.Screen 
-        name="TimeSelect" 
+      <Stack.Screen
+        name="TimeSelect"
         component={TimeSelectScreen}
         options={{ headerShown: false }}
       />
-      <Stack.Screen 
-        name="FlexibleGoal" 
+      <Stack.Screen
+        name="FlexibleGoal"
         component={FlexibleGoalScreen}
-        options={{ title: '유연한 목표' }}
+        options={{ title: "필수 목표" }}
       />
-      <Stack.Screen 
-        name="Retrospect" 
+      <Stack.Screen
+        name="Retrospect"
         component={RetrospectScreen}
-        options={{ title: '회고 작성' }}
+        options={{ title: "회고 작성" }}
       />
     </Stack.Navigator>
   );
